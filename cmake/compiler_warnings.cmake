7e8b71e4ccfd7633b7cd7fc42829c2f014abc6a9
andwati <andwatiian@gmail.com>
2025-12-20 21:20:33 +0300

    remove CMake configuration files and related setup

--- cmake/compiler_warnings.cmake
+++
@@ -1,6 +0,0 @@
-# Custom warnings setup
-if (MSVC)
-    add_compile_options(/W4 /permissive-)
-else()
-    add_compile_options(-Wall -Wextra -Wpedantic -Wshadow -Wconversion -Wsign-conversion)
-endif()
